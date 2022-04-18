160322c6081ea4d231d99793cdf8368a2dd04b3c
monse <monsen.j@northeastern.edu>
2022-04-18 19:02:26 -0400

    Word class commit

---
+++ venv/word.py
@@ -0,0 +1,30 @@
+class Word:
+    """Class"""
+    def __init__(self, word, dict, line):
+        self.word = word
+        self.dict = dict
+        self.line = line
+
+    def checkWord(self):
+        if self.checkSpelled():
+            return word
+        ist = self.findAlternatives()
+        return self.chooseAlternative(ist, self.line)
+
+    def checkSpelled(self):
+        if self.word in self.dict:
+            return True
+        return False
+
+    def findAlternatives(self):
+        s = self.word
+
+
+    def chooseAlternative(self, ist, line):
+        print("\n The incorrectly spelled word is and its line: " + self.word)
+        print(self.line)
+        print("Here is a list of possible intended words, along with an index showing how close they are to the"
+              " original. Lower is closer to the original")
+        print(ist)
+        i = input("Enter the index corresponding to the word you want to replace the misspelled one.")
+        return ist[i]
